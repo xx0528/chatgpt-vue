4692d88471c249b1abbed98be54669fe7ba8325b
minigame <minigame6668@gmail.com>
2023-04-16 19:30:12 +0800

    textare

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,9 +4,9 @@
       class="flex flex-nowrap fixed w-full items-baseline top-0 px-6 py-4 bg-gray-100"
     >
       <div class="text-2xl font-bold">ChatGPT</div>
-      <div class="ml-4 text-sm text-gray-500">
+      <!-- <div class="ml-4 text-sm text-gray-500">
         基于 OpenAI 的 ChatGPT 自然语言模型人工智能对话
-      </div>
+      </div> -->
       <div
         class="ml-auto px-3 py-2 text-sm cursor-pointer hover:bg-white rounded-md"
         @click="clickConfig()"
@@ -40,12 +40,13 @@
         请输入 API Key：
       </div>
       <div class="flex">
-        <input
+        <textarea
           class="input"
+          rows="8"
           :type="isConfig ? 'password' : 'text'"
-          :placeholder="isConfig ? 'sk-xxxxxxxxxx' : '请输入'"
+          :placeholder="isConfig ? 'sk-xxxxxxxxxx' : '请输入内容 ctrl+enter 快捷键发送'"
           v-model="messageContent"
-          @keydown.enter="isTalking || sendOrSave()"
+          @keydown.ctrl.enter="isTalking || sendOrSave()"
         />
         <button class="btn" :disabled="isTalking" @click="sendOrSave()">
           {{ isConfig ? "保存" : "发送" }}
@@ -74,19 +75,11 @@ const roleAlias = { user: "ME", assistant: "ChatGPT", system: "System" };
 const messageList = ref<ChatMessage[]>([
   {
     role: "system",
-    content: "你是 ChatGPT，OpenAI 训练的大型语言模型，尽可能简洁地回答。",
+    content: "",
   },
   {
     role: "assistant",
-    content: `你好，我是AI语言模型，我可以提供一些常用服务和信息，例如：
-
-1. 翻译：我可以把中文翻译成英文，英文翻译成中文，还有其他一些语言翻译，比如法语、日语、西班牙语等。
-
-2. 咨询服务：如果你有任何问题需要咨询，例如健康、法律、投资等方面，我可以尽可能为你提供帮助。
-
-3. 闲聊：如果你感到寂寞或无聊，我们可以聊一些有趣的话题，以减轻你的压力。
-
-请告诉我你需要哪方面的帮助，我会根据你的需求给你提供相应的信息和建议。`,
+    content: ``,
   },
 ]);
 
